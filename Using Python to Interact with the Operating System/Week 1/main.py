8b0ebeaeebb7bfc7f9ed94de7a06b6a0a2528337
Anastasia Lunina <lunina.anastasiya@gmail.com>
2022-07-15 21:59:43 -0400

    Initial commit

---
+++ Using Python to Interact with the Operating System/Week 1/main.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import requests
+import socket
+
+localhost = socket.gethostbyname('localhost')
+request = requests.get("http://www.google.com")
+
+
+def check_localhost():
+    if localhost == "127.0.0.1":
+        return True
+
+
+def check_connectivity():
+    if request.status_code == 200:
+        return True
+
+# !/usr/bin/env python3
+from network import *
+import shutil
+import psutil
+
+
+def check_disk_usage(disk):
+    """Verifies that there's enough free space on disk"""
+    du = shutil.disk_usage(disk)
+    free = du.free / du.total * 100
+    return free > 20
+
+
+def check_cpu_usage():
+    """Verifies that there's enough unused CPU"""
+    usage = psutil.cpu_percent(1)
+    return usage < 75
+
+
+# If there's not enough disk, or not enough CPU, print an error
+if not check_disk_usage('/') or not check_cpu_usage():
+    print("ERROR!")
+elif check_localhost() and check_connectivity():
+    print("Everything ok")
+else:
+    print("Network checks failed")
