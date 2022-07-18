c1a074ef844841d81987eaadba7a4d4c611d8e38
Anastasia Lunina <lunina.anastasiya@gmail.com>
2022-07-17 23:48:16 -0400

    Add Weeks 4,5,6, modify weeks 1,2,3

---
+++ Using Python to Interact with the Operating System/Week 6 Editing Files Using Substrings/findJane.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+> oldFiles.txt
+
+files=$(grep " jane " ../data/list.txt | cut -d' ' -f3)
+for f in $files; do
+  if [ -e $HOME$f ]; then
+   echo $HOME$f >>oldFiles.txt;
+  fi
+done
